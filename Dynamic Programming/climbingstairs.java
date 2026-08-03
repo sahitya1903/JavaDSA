86d6ddb3125ace39e5cdd08edd1345bc4a35a635
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-08-03 18:43:48 +0530

    Add implementation for counting ways to climb stairs using dynamic programming

---
+++ Dynamic Programming/climbingstairs.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class climbingstairs {
+    //Memoization + Recursion
+    public static int countways(int n, int [] ways){
+        if (n<0) return 0;
+        if(n==0) return 1;
+        if(ways[n]!=-1) return n;
+
+        ways[n]= countways(n-1, ways)+countways(n-2, ways);
+        return ways[n];
+    }
+    public static void main(String[] args) {
+        int n=100;
+        int ways[]=new int[n+1];
+        Arrays.fill(ways,-1);
+        System.out.println(countways(n,ways));
+    }
+}
