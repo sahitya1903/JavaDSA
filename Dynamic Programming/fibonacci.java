d3f0ea78fb71b6669075c3e2457af973ed6b86f7
Sahitya Kushwaha <kushwahasahitya909@gmail.com>
2026-08-03 17:44:53 +0530

    Add implementation of Fibonacci sequence using dynamic programming

---
+++ Dynamic Programming/fibonacci.java
@@ -0,0 +1,11 @@
+public class fibonacci {
+    // Tabulation
+    public static void main(String[] args) {
+        int n = 5;
+        int dp[] = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[n - 2];
+        }
+        System.out.println(dp[n]);
+    }
+}
